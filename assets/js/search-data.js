97ee38ef1f3eff836d909da023195ea4a786a022
EshaanJaiswal <eshaanjaiswal06@gmail.com>
2025-03-31 19:38:15 +0000

    Deploying to gh-pages from @ EshaanJaiswal/EshaanJaiswal.github.io@b8602f342b4ebdcd769ae36730dbb1557c9e10d1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -518,13 +518,6 @@ ninja.data = [{
           window.open("https://www.linkedin.com/in/eshaanjaiswal", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
         id: 'social-x',
         title: 'X',
         section: 'Socials',
